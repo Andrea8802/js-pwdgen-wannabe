ba79b3dc4390278025d1cad21dd8f0165c9edc50
Andrea8802 <andreabonaventura802@gmail.com>
2022-11-04 15:11:58 +0100

    All Project Completed

--- js/main.js
+++ js/main.js
@@ -7,20 +7,14 @@ const number = 21;
 // Chiedere all'utente il proprio nome
 firstName = prompt("Inserisci il tuo nome");
 
-console.log(firstName);
-
 // Chiedere all'utente il proprio cognome
 lastName = prompt("Inserisci il tuo cognome");
-console.log(lastName);
-
 
 // Chiedere all'utente il proprio colore preferito
 favColor = prompt("Inserisci il tuo colore preferito");
-console.log(favColor);
-
 
 // Mettere insieme tutti i dati
 password = firstName + lastName + favColor + number
 
 // Output
-console.log(password);
+document.getElementById("password").innerHTML = password
